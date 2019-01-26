e3776d938a083a7a01a9141e8bf24b20712b73fc
maxyee <eyaminkhan00@gmail.com>
2019-01-26 13:52:55 +0600

    Added the template file in a organized way and shorten the repeted code by using layout.html file

--- flask_blog/flaskblog.py
+++ flask_blog/flaskblog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 
@@ -17,17 +17,18 @@ posts = [
     }
 ]
 
+
 @app.route("/")
-def hello():
-    return "<h1>Hello World!</h1>"
+def Start():
+    return render_template('start.html')
 
 @app.route("/Home")
 def Home():
-    return render_template('home.html', posts = posts)
+    return render_template('home.html', posts = posts, title='Home')
 
 @app.route("/About")
 def About():
-    return "This is About page"
+    return render_template('about.html', title = 'About')
 
 
 if __name__ == "__main__":
