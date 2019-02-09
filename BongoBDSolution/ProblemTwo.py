c7c9cb34ef93fd6541189e4c68e9a7b696fdc628
maxyee <eyaminkhan00@gmail.com>
2019-02-10 04:29:08 +0600

    Solved ProblemTwo but there is some str related error

--- BongoBDSolution/ProblemTwo.py
+++ BongoBDSolution/ProblemTwo.py
@@ -5,37 +5,78 @@ class Person(object):
         self.last_name = last_name
         self.father = father
 
-person_a = Person("User","1","father")
-person_b = Person("User","2",person_a)
 
+person_a = Person("User", "1", "father")
+person_b = Person("User", "2", person_a)
+
+
+"""
+// testing purpose print
+ 
+print(person_b.__dict__.keys())
+print(person_b.__dict__)
+list = person_b.__dict__
+print(person_b.__dict__.values())
+
+"""
+
+# dictionary initialized with variable a
 a = {
     'key1': 1,
-    'key2':{
-        'key3':1,
-        'key4':{
-            'key5':4,
-            "user":person_b
+    'key2': {
+        'key3': 1,
+        'key4': {
+            'key5': 4,
+            "user": person_b
         }
     }
 }
 
+depth = 1
+for key, value in person_b.__dict__.items():
+    if type(value) is type(person_a):
+        for k,v in person_a.__dict__.items():
+            print(k,v,depth)
+            depth += 1
+    else:
+        print(key,value,depth)
+        depth += 1
+
 index = 1
+
+
 def print_depth(data):
      for key, value in data.items():
         if type(value) is dict:
             yield(key, value, index)
             yield from print_depth(value)
-        elif type(value) is object.__doc__:
-            for key in person_b.items():
-                yield(key, value, index)
-                yield from print_depth(value)
+        elif type(value) is type(person_b):
+            yield (key, value, index)
+            for key, value in person_b.__dict__.items():
+                yield (key, value, index)
+                if type(value) is type(person_a):
+                    for key, value in person_a.__dict__.items():
+                        yield (key, value, index)
+            yield from print_depth(value)
         else:
             yield(key, value, index)
+
+
+# or type(person_a) or type(person_b)
 
 for key, value, index in print_depth(a):
     print(key,index)
     if type(value) is dict:
         index += 1
+    elif type(value) is type(person_a):
+        index += 1
+    elif type(value) is type(person_b):
+        index += 1
+    else:
+        index = index
+
+
+
 
 """
 Sample Output:
