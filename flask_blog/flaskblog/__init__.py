986101c6d3cc108bf167de6356dd168b84d98963
maxyee <eyaminkhan00@gmail.com>
2019-04-12 05:03:51 +0600

    Solved the user authentication for the blog site and also made a view called account and validate that page for the authencated user

--- flask_blog/flaskblog/__init__.py
+++ flask_blog/flaskblog/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
@@ -8,5 +9,8 @@ app.config['SECRET_KEY'] = 'e62997d006694221ab21fdc90687ce9e'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from flaskblog import routes
